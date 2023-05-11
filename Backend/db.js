c902e9d364ebde8e4dd9035e0ce54f8af1b3a75b
SAYALI JADHAV <jadhavsayalip2316@gmail.com>
2023-05-11 10:17:59 +0530

    done

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-
-const connection=mongoose.connect("mongodb+srv://sayali:sayalijadhav@cluster0.vgf4quo.mongodb.net/Amazon?retryWrites=true&w=majority")
+require("dotenv").config()
+const connection=mongoose.connect(process.env.mongoURL)
 
 module.exports={connection}
--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ const {mobileRouter}=require("./routes/mobile.routes")
 const {buyerRouter}=require("./routes/buyer.routes")
 const {soldRouter}=require("./routes/sold.route")
 const cors=require("cors")
+require("dotenv").config()
 
 const app=express()
 app.use(express.json())
@@ -15,11 +16,11 @@ app.use("/mobile",mobileRouter)
 app.use("/buy",buyerRouter)
 app.use("/sold",soldRouter)
 
-app.listen(4500,async()=>{
+app.listen(process.env.port,async()=>{
     try {
         await connection
         console.log("Connected to db")
-        console.log("Server is listening at port 4500")
+        console.log(`Server is listening at port ${process.env.port}`)
     } catch (error) {
         console.log(error)
     }
